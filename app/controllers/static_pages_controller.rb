47f0c6ac09a9ad8573698801a9eff9e7b9c94049
levonavetyan <levon.avetyan.y@tumo.org>
2019-12-24 19:26:41 +0400

    Tweets

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @tweets = Tweet.all
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :room_messages
+  resources :rooms
   devise_for :users
+  resources :tweets
+
   root 'static_pages#home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
