25d34daf9a573b2c2d9c33eddfcf17e6a08daedf
gayane1234martirosyan4321 <gayane.m3.y@tumo.org>
2019-12-26 19:29:33 +0400

    ChatStyles2

--- app/javascript/room.js
+++ app/javascript/room.js
@@ -1,5 +1,15 @@
 $(function() {
-    $('#new_room_message').on('ajax:success', function(a, b,c ) {
-      $(this).find('input[type="text"]').val('');
-    });
-  });
+  // alert('ad')
+  
+  
+
+
+  $('.send-button').on('click',()=>{
+    let valu = $('.chat-input').val();
+    
+    $( ".chat_text_area" ).append("<p class='message-content'>"+valu+"</p>");
+
+    // $('.chat-input').val(''); 
+  })
+});
+
