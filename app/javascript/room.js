5be8a1b883e5c06c1cd9de635b4461c488632e01
gayane1234martirosyan4321 <gayane.m3.y@tumo.org>
2019-12-27 15:56:16 +0400

    Comment out js

--- app/javascript/room.js
+++ app/javascript/room.js
@@ -1,15 +1,20 @@
-$(function() {
-  // alert('ad')
+// $(function() {
+//   // alert('ad')
   
   
 
 
-  $('.send-button').on('click',()=>{
-    let valu = $('.chat-input').val();
+//   $('.send-button').on('click',()=>{
+//     let valu = $('.chat-input').val();
     
-    $( ".chat_text_area" ).append("<p class='message-content'>"+valu+"</p>");
+//     $( ".chat_text_area" ).append("<p class='message-content'>"+valu+"</p>");
 
-    // $('.chat-input').val(''); 
-  })
+//     // $('.chat-input').val(''); 
+//   })
+// });
+
+$(function() {
+  $('#new_room_message').on('ajax:success', function(a, b,c ) {
+    $(this).find('input[type="text"]').val('');
+  });
 });
-
