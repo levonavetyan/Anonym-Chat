a92c6d59704a1e1af4e18352c808f078f84313a2
levonavetyan <levon.avetyan.y@tumo.org>
2020-01-09 17:07:46 +0400

    Fix error

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2020_01_08_121436) do
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "tweet_content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "subject"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,11 +54,6 @@ ActiveRecord::Schema.define(version: 2020_01_08_121436) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
-  add_foreign_key "rooms", "users"
-=======
   create_table "votes", force: :cascade do |t|
     t.string "votable_type"
     t.integer "votable_id"
@@ -66,8 +70,8 @@ ActiveRecord::Schema.define(version: 2020_01_08_121436) do
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
-  add_foreign_key "room_messages", "rooms"
-  add_foreign_key "room_messages", "users"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "rooms", "users"
   add_foreign_key "tweets", "users"
->>>>>>> Page_Styles
 end
